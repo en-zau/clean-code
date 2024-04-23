932b1322020acb41a68c7ffe0c85d98ec755ad31
en-zau <gouerec.enzo@gmail.com>
2024-04-23 17:13:44 +0200

    ex2/ finish exercice 2

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,24 @@
 package ex2;
 
+/**
+ * Représente un livret A.
+ */
 public class LivretA extends CompteBancaire {
+    private double tauxRemuneration;
 
-    public LivretA(String type, double solde, double tauxRemuneration) {
-        super(type, solde, 0, tauxRemuneration);
+    public LivretA(double solde, double tauxRemuneration) {
+        super(solde, 0); // Initialisation du découvert à zéro, car un livret A ne peut pas être à découvert
+        this.tauxRemuneration = tauxRemuneration;
     }
 
+    @Override
+    public void debiterMontant(double montant) {
+        if (this.solde - montant >= 0) {
+            this.solde -= montant;
+        }
+    }
+
+    public void appliquerRemuAnnuelle() {
+        this.solde += this.solde * (this.tauxRemuneration / 100);
+    }
 }
