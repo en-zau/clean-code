edff562a2c0e978f336e2bfcd52e88149e2b9235
en-zau <gouerec.enzo@gmail.com>
2024-04-23 17:20:27 +0200

    ex4/ finish exercice 4

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -5,15 +5,19 @@ import java.util.Date;
 
 public class DateUtils {
 
-    public static String format(String pattern, Date date) {
+    private static final String DEFAULT_PATTERN = "dd/MM/yyyy HH:mm:ss";
 
-        SimpleDateFormat formateur = new SimpleDateFormat(pattern);
+    public static String format(String pattern, Date date) {
+        SimpleDateFormat formateur = new SimpleDateFormat(getPattern(pattern));
         return formateur.format(date);
     }
 
     public static String formatDefaut(Date date) {
-
-        SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        SimpleDateFormat formateur = new SimpleDateFormat(DEFAULT_PATTERN);
         return formateur.format(date);
+    }
+
+    private static String getPattern(String pattern) {
+        return pattern != null ? pattern : DEFAULT_PATTERN;
     }
 }
