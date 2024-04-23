cac7df36fc25d25c5192b3e0bfac5716245d774d
en-zau <gouerec.enzo@gmail.com>
2024-04-23 16:58:22 +0200

    ex1/ finish exercice 1

--- src/main/java/ex1/calcul.java
+++ src/main/java/ex1/calcul.java
@@ -1,12 +1,16 @@
 package ex1;
 
-public class calcul {
+/**
+ * Classe contenant des méthodes de calcul.
+ */
+public class Calcul {
 
-    public int get1(int a, int b) {
+    public int additionner(int a, int b) {
         return a + b;
     }
 
-    public int get2(int a, int b) {
+    public int soustraire(int a, int b) {
         return a - b;
     }
 }
+
--- src/main/java/ex1/entreprise.java
+++ src/main/java/ex1/entreprise.java
@@ -2,16 +2,30 @@ package ex1;
 
 import java.util.Date;
 
-public class entreprise {
+/**
+ * Classe représentant une entreprise.
+ */
+public class Entreprise {
 
-    public static final int capitalMax = 3000000;
-    public int Siret;
-    public String Nom;
+    /** Capital maximum de l'entreprise. */
+    public static final int CAPITAL_MAX = 3000000;
+
+    /** Numéro SIRET de l'entreprise. */
+    public int siret;
+
+    /** Nom de l'entreprise. */
+    public String nom;
+
+    /** Adresse de l'entreprise. */
     public String adresse;
-    public Date date_Creation;
 
-    public void Afficher_statut() {
+    /** Date de création de l'entreprise. */
+    public Date dateCreation;
 
+    /**
+     * Affiche le statut de l'entreprise.
+     */
+    public void afficherStatut() {
+        System.out.println(nom, adresse)
     }
-
 }
